b634e41ec3a368ec32c9c4af439cee94e6a1caf7
Nayra-pixel <nayrasduarte@gmail.com>
2025-12-02 18:07:09 -0300

    inicio tentando

--- app/src/main/java/br/com/etecia/kimcoffee/InicioActivity.java
+++ app/src/main/java/br/com/etecia/kimcoffee/InicioActivity.java
@@ -1,24 +1,50 @@
 package br.com.etecia.kimcoffee;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class InicioActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.inicio_layout);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        bottomNav = findViewById(R.id.idBottomNavMenu);
+
+        loadFragment(new CoffeeFragment());
+
+        bottomNav.setOnItemSelectedListener(item -> {
+            loadFragment(createFragment(item.getItemId()));
+            return true;
         });
+    }
+    private Fragment createFragment(int idPosition) {
+        switch (idPosition) {
+            case 0:
+                return new CoffeeFragment();
+
+            case 1:
+                return new MuffinFragment();
+
+            case 2:
+                return new TeaFragment();
+
+            case 3:
+                return new DonutsFragment();
+
+            default:
+                return new CoffeeFragment();
+        }
+    }
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.idFrameContainer, fragment)
+                .commit();
     }
 }
